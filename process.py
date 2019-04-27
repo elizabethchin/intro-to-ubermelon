a8dc86715ea573914fee22af8585654ab5af3d09
vagrant <vagrant@vagrant.vm>
2019-04-27 05:07:22 +0000

    Change comments

--- process.py
+++ process.py
@@ -1,15 +1,18 @@
 log_file = open("um-server-01.txt")
-#opens text file
+#creates a file object from text file 
 
 #defines a function called sales reports
 def sales_reports(log_file):
+"""Prints sales information for Monday."""
+
+    #get each line in log_file
     for line in log_file:
         #loops through each line
         line = line.rstrip()
         #removes white space
         day = line[0:3]
         #keeps first three indicies of day
-        if day == "Tue":
+        if day == "Mon":
             #if day equals to value it will print it
             print(line)
 
